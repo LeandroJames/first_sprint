456d95e2056acff5df734e53c2694bde10276e65
Leandro <leandroespanol@gmail.com>
2023-01-26 17:41:54 +0100

    Pizza complete!

--- sp2/pizzeria/queries_pizza.sql
+++ sp2/pizzeria/queries_pizza.sql
@@ -1,19 +1,25 @@
-SELECT products.* FROM pizzeria.products
+SELECT products.*
+FROM pizzeria.products
 WHERE
-    products.`idProducts` IN 
-        (SELECT `order item`.`idProduct` FROM pizzeria.`order item`)
-    AND `Type`= "Drink";
+    products.`idProducts` IN (
+        SELECT
+            `order item`.`idProduct`
+        FROM pizzeria.`order item`
+        WHERE
+            `order item`.`idOrder` IN (
+                SELECT `order`.`idOrder`
+                FROM pizzeria.`order`
+                WHERE `order`.`idCustomer` IN (
+                        SELECT
+                            `customers`.`idCustomers`
+                        FROM pizzeria.`customers`
+                        WHERE
+                            `customers`.`City` = "Barcelona"
+                    )
+            )
+    )
+    AND `Type` = "Drink";
 
-
- ulleres_cul_ampolla.sales WHERE CustomerID = 1;
-
-SELECT * FROM ulleres_cul_ampolla.sales;
-SELECT * FROM ulleres_cul_ampolla.sales WHERE `Sold by` = "Aethelbert";
-
-SELECT suppliers.* FROM ulleres_cul_ampolla.suppliers
-WHERE suppliers.SupplierID IN
-(SELECT glasses.SupplierID FROM ulleres_cul_ampolla.glasses
-WHERE glasses.GlassesID IN (SELECT sales.GlassesID FROM ulleres_cul_ampolla.sales));
-
-Llista quants productes de tipus “Begudes”. s'han venut en una determinada localitat.
-Llista quantes comandes ha efectuat un determinat empleat/da.
+SELECT `order`.*
+FROM pizzeria.`order`
+WHERE `Order`.`Delivered by`=2
