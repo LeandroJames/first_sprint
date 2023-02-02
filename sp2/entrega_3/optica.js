e0bfc6a786095502c91739f7c59fa916d1a3d719
LeandroJames <leandroespanol@gmail.com>
2023-02-02 09:16:50 +0100

    very little

--- sp2/entrega_3/optica.js
+++ sp2/entrega_3/optica.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://leandroespanol:SilvioAstier23@cluster0.og3s1v9.mongodb.net/optica";
+var url =;
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
